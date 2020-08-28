830a850766a1108328e92ee6d636e827c86198c4
Uglyshiji <69787408+Uglyshiji@users.noreply.github.com>
2020-08-28 11:24:32 +0800

    界面搭建,跳过了登录验证,已经和后端结合! (#1)
    Co-authored-by: 蒋福松 <1161126269@qq.com>

--- src/main.js
+++ src/main.js
@@ -5,19 +5,22 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
 import axios from 'axios'
+import qs from 'qs'
 
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = '/'
+// axios.defaults.baseURL = '/'
+Vue.use(router)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.$http = axios  //axios配置
+Vue.prototype.$qs = qs;
+// Vue.prototype.$url = 'http://localhost:8080/SCCS'
 // 需要与后端对接时注释掉
 // require('./mock')
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
+
