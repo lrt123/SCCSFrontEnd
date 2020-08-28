814004d2063988684b6b0117be7b0a0e229840e8
蒋福松 <1161126269@qq.com>
2020-08-28 21:23:45 +0800

    用户管理功能基本实现

--- src/components/User/GetAllUser.vue
+++ src/components/User/GetAllUser.vue
@@ -28,80 +28,203 @@
       <el-table-column prop="roles[0].rolename" label="身份" width="120"></el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
+          <el-button @click="deleteUserInfo(scope.row)" type="text" size="small">删除</el-button>
+          <el-button @click="editUserInfo(scope.row)" type="text" size="small">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination layout="prev, pager, next" :total="100"></el-pagination>
+    <el-pagination
+      layout="prev, pager, next"
+      @current-change="handleCurrentChange"
+      :current-page.sync="currentPage"
+      :total="totalNum"
+    ></el-pagination>
+    <el-dialog title="修改信息" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="编号">
+          <el-input style="width:300px" v-model="form.id"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名">
+          <el-input style="width:300px" v-model="form.userInfo.username"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄">
+          <el-input-number
+            v-model="form.userInfo.age"
+            controls-position="right"
+            :min="1"
+            :max="100"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-radio v-model="form.userInfo.sex" label="男">男</el-radio>
+          <el-radio v-model="form.userInfo.sex" label="女">女</el-radio>
+        </el-form-item>
+        <el-form-item label="手机号">
+          <el-input style="width:300px" v-model="form.userInfo.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="角色">
+          <el-select v-model="form.roles[0].roleid" placeholder="请选择角色">
+            <el-option label="管理员" value="R0001"></el-option>
+            <el-option label="老师" value="R0003"></el-option>
+            <el-option label="学生" value="R0002"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="updateUserInfo">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-  export default {
-    methods: {
-      onsubmit() {
-        console.log('name: '+this.formLabelAlign.name );
-        console.log('value: '+this.formLabelAlign.value );
-        this.$http.get('',this.formLabelAlign).then(res => {
-          console.log("返回数据");
+export default {
+  methods: {
+    onsubmit() {
+      console.log("onsubmit");
+      console.log("name: " + this.formLabelAlign.name);
+      console.log("value: " + this.formLabelAlign.value);
+      this.$http
+        .get("/users/getUsersByCondition", {
+          params: {
+            condition: this.formLabelAlign.value,
+            value: this.formLabelAlign.name,
+          },
         })
-        console.log('');
-      },
-
-      handleClick(row) {
-        console.log(row);
-      },
+        .then((res) => {
+          console.log(res.data.data);
+          this.tableData = res.data.data;
+        });
     },
-    mounted(){
-      this.$http.get('/users/getAllUser').then(res => {
-        console.log(res.data)
-        this.tableData  = res.data.data;
+    deleteUserInfo(row) {
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       })
-    },
-    data() {
-      return {
-        labelPosition: "right",
-        formLabelAlign: {
-          name: "",
-          value: ''
-        },
-        options: [
-          {
-            value: "id",
-            label: "编号",
-          },
-          {
-            value: "username",
-            label: "姓名",
-          },
-          {
-            value: "phone",
-            label: "手机号",
-          }
-        ],
-        tableData: [
-          {
-            id: null,
-            roles: [
-              {
-                rolename: null,
-              }
-            ],
-            userInfo: {
-              age: null,
-              phone: null,
-              sex: null,
-              username: null,
+        .then(() => {
+          this.$http.get("/users/deleteUser", { 
+            params: {
+              id : row.id
             }
-          }
-        ],
-      };
+          }).then((res) => {
+            if (res.data.code === 200) {
+              this.$message({
+                type: "success",
+                message: "删除成功!",
+              });
+            }else{
+              this.$message({ type: "info", message: "删除失败" });
+            }
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
     },
-  };
+    handleCurrentChange() {
+      console.log("updateUserInfo");
+      this.$http
+        .get("/users/getAllUser", { params: { currentPage: this.currentPage } })
+        .then((res) => {
+          this.tableData = res.data.data.list;
+          this.totalNum = res.data.data.totalPage * 10;
+        });
+      console.log(this.currentPage);
+    },
+    editUserInfo(row) {
+      console.log(row);
+      this.dialogFormVisible = true;
+      this.form = row;
+    },
+    updateUserInfo() {
+      console.log("updateUserInfo");
+      this.$http.post("/users/updateUser", this.form).then((res) => {
+        if (res.data.code === 200) {
+          this.$message({ message: "更新成功", type: "success" });
+          this.dialogFormVisible = false;
+        } else {
+          this.$message({ type: "info", message: "更新失败" });
+        }
+      });
+    },
+  },
+  mounted() {
+    console.log("mounted");
+    this.$http.get("/users/getAllUser?currentPage=1").then((res) => {
+      console.log(res.data);
+      this.tableData = res.data.data.list;
+      this.totalNum = res.data.data.totalPage * 10;
+    });
+  },
+  data() {
+    return {
+      labelPosition: "right",
+      totalNum: 10,
+      currentPage: 1,
+      formLabelAlign: {
+        name: "",
+        value: "id",
+      },
+      dialogFormVisible: false,
+      form: {
+        id: "",
+        userInfo: {
+          id: "",
+          username: "",
+          age: "",
+          sex: "",
+          phone: "",
+        },
+        roles: [
+          {
+            roleid: "",
+            rolename: "",
+            menus: null,
+          },
+        ],
+      },
+      formLabelWidth: "120px",
+      options: [
+        {
+          value: "id",
+          label: "编号",
+        },
+        {
+          value: "username",
+          label: "姓名",
+        },
+        {
+          value: "phone",
+          label: "手机号",
+        },
+      ],
+      tableData: [
+        {
+          id: null,
+          roles: [
+            {
+              rolename: null,
+            },
+          ],
+          userInfo: {
+            age: null,
+            phone: null,
+            sex: null,
+            username: null,
+          },
+        },
+      ],
+    };
+  },
+};
 </script>
 <style scoped>
-  span {
-    font-size: 16px;
-  }
+span {
+  font-size: 16px;
+}
 </style>
